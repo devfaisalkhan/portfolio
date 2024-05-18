a62be4ad465b942e798d78469de6c2a62b538432
Faisal khan <dev.faisalkhan@gmail.com>
2024-05-18 21:25:59 +0500

    ref()

--- web/src/app/components/about/about.component.ts
+++ web/src/app/components/about/about.component.ts
@@ -8,9 +8,54 @@ declare var createTree: any;
   selector: 'app-about',
   standalone: true,
   imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.scss',
-  encapsulation: ViewEncapsulation.None
+  template: `
+    <div class="tree">
+    <div class="buttons">
+        <div class="refresh" (click)="onRecenterClicked()">
+            <p>Recenter tree</p>
+        </div>
+    </div>
+    
+    <div id="tree-container"></div>
+</div>
+
+  `,
+  styles: `
+    app-about{
+    .tree{
+        position: relative;
+        margin: 0 20px;
+        .buttons{
+            z-index: 999;
+            cursor: pointer;
+            display: flex;
+            justify-content: center;
+    
+            .refresh{
+                z-index: 999;
+                position: absolute;
+                left: 0;
+                top: 50%;
+                bottom: 0;
+                text-decoration: underline;
+            }
+        }
+    }
+
+}
+
+ 
+  
+  #tree-container {
+    height: 100vh;
+    z-index: 9;
+    position: relative;
+
+  }
+
+  `,
+  encapsulation: ViewEncapsulation.None,
+
 })
 
 export class AboutComponent implements OnInit {
